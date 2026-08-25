6c689f34c093bfbd019b31f8e6b0ed85bb7eb71c
usepgnwan <usepgnwan@gmail.com>
2026-08-25 07:27:06 +0700

    fix seo

--- server/app/controller/sitemap.go
+++ server/app/controller/sitemap.go
@@ -11,43 +11,33 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func GenerateSitemap(c echo.Context) error {
-	baseUrl := "https://tryout.rifayaeducation.com"
-	
-	// Static routes
+const baseUrl = "https://tryout.rifayaeducation.com"
+
+func GenerateSitemapIndex(c echo.Context) error {
+	xmlLines := []string{
+		`<?xml version="1.0" encoding="UTF-8"?>`,
+		`<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
+	}
+	sitemaps := []string{
+		"/page-sitemap.xml",
+		"/package-sitemap.xml",
+		"/blog-sitemap.xml",
+	}
+	for _, sm := range sitemaps {
+		xmlLines = append(xmlLines, fmt.Sprintf(`  <sitemap>
+    <loc>%s%s</loc>
+  </sitemap>`, baseUrl, sm))
+	}
+	xmlLines = append(xmlLines, `</sitemapindex>`)
+	return c.Blob(http.StatusOK, "application/xml", []byte(strings.Join(xmlLines, "\n")))
+}
+
+func GeneratePageSitemap(c echo.Context) error {
 	paths := []string{
 		"/",
-		"/login",
-		"/register",
-		"/forgot-password",
 		"/paket",
-		"/keranjang",
-		"/checkout",
-		"/profile",
-		"/pembelian",
-		"/dashboard",
-		"/riwayat",
-		"/latihan",
 		"/blog",
 		"/kontak",
-	}
-
-	// Dynamic Packages
-	var packages []model.Package
-	connection.DB.Where("status != ?", "draft").Find(&packages)
-	for _, pkg := range packages {
-		if pkg.Slug != "" {
-			paths = append(paths, "/paket/"+pkg.Slug)
-		}
-	}
-
-	// Dynamic Blogs/Articles
-	var artikels []model.Artikel
-	connection.DB.Where("status != ?", "draft").Find(&artikels)
-	for _, art := range artikels {
-		if art.Slug != "" {
-			paths = append(paths, "/blog/"+art.Slug)
-		}
 	}
 
 	xmlLines := []string{
@@ -60,7 +50,7 @@ func GenerateSitemap(c echo.Context) error {
 		if p == "/" {
 			priority = "1.0"
 		}
-		
+
 		xmlLines = append(xmlLines, fmt.Sprintf(`  <url>
     <loc>%s%s</loc>
     <changefreq>weekly</changefreq>
@@ -69,7 +59,51 @@ func GenerateSitemap(c echo.Context) error {
 	}
 
 	xmlLines = append(xmlLines, `</urlset>`)
-	xmlOutput := strings.Join(xmlLines, "\n")
+	return c.Blob(http.StatusOK, "application/xml", []byte(strings.Join(xmlLines, "\n")))
+}
 
-	return c.Blob(http.StatusOK, "application/xml", []byte(xmlOutput))
+func GeneratePackageSitemap(c echo.Context) error {
+	var packages []model.Package
+	connection.DB.Where("status != ?", "draft").Find(&packages)
+
+	xmlLines := []string{
+		`<?xml version="1.0" encoding="UTF-8"?>`,
+		`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
+	}
+
+	for _, pkg := range packages {
+		if pkg.Slug != "" {
+			xmlLines = append(xmlLines, fmt.Sprintf(`  <url>
+    <loc>%s/paket/%s</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`, baseUrl, pkg.Slug))
+		}
+	}
+
+	xmlLines = append(xmlLines, `</urlset>`)
+	return c.Blob(http.StatusOK, "application/xml", []byte(strings.Join(xmlLines, "\n")))
+}
+
+func GenerateBlogSitemap(c echo.Context) error {
+	var artikels []model.Artikel
+	connection.DB.Where("status != ?", "draft").Find(&artikels)
+
+	xmlLines := []string{
+		`<?xml version="1.0" encoding="UTF-8"?>`,
+		`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`,
+	}
+
+	for _, art := range artikels {
+		if art.Slug != "" {
+			xmlLines = append(xmlLines, fmt.Sprintf(`  <url>
+    <loc>%s/blog/%s</loc>
+    <changefreq>weekly</changefreq>
+    <priority>0.8</priority>
+  </url>`, baseUrl, art.Slug))
+		}
+	}
+
+	xmlLines = append(xmlLines, `</urlset>`)
+	return c.Blob(http.StatusOK, "application/xml", []byte(strings.Join(xmlLines, "\n")))
 }
